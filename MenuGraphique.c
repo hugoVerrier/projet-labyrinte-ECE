16d674210ea3bcab9d1b5b196c53ab0743bc7079
eliotrineau <eliot.rineau@edu.ece.fr>
2022-12-19 19:57:42 +0100

    affichage fond mais erreur dans le chemin d'acces

--- MenuGraphique.c
+++ MenuGraphique.c
@@ -9,15 +9,29 @@ int fenetreMenu(){
     hauteur = GetScreenHeight();
     InitWindow(largeur,hauteur,"Menu");
     ToggleFullscreen();
+    //Texture2D fond = LoadTexture("/Users/eliotrineau/CLionProjects/projet-labyrinte-ECE/ImagesMenu/fond.png");
+    Texture2D fond = LoadTexture("./ImagesMenu/fond.png");
+
+    SetTargetFPS(30);
     while (!WindowShouldClose()){
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
-        DrawRectangle(GetScreenWidth()/2,GetScreenHeight()/2,GetScreenWidth()/2,GetScreenHeight(),BLACK);
-        DrawText("Test", GetScreenWidth()/2-24,GetScreenHeight()/2,20,WHITE);
+        DrawRectangle((GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8,GetScreenWidth()/4,GetScreenHeight()/12,RED);
+        DrawRectangle((GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8+GetScreenWidth()/8,GetScreenWidth()/4,GetScreenHeight()/12,RED);
+        DrawRectangle((GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8+2*GetScreenWidth()/8,GetScreenWidth()/4,GetScreenHeight()/12,RED);
+        DrawRectangle((GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8+3*GetScreenWidth()/8,GetScreenWidth()/4,GetScreenHeight()/12,RED);
+        DrawTextureEx(fond,(Vector2){0,0},0.0f,1.0f,WHITE);
+
         if(IsKeyPressed(KEY_ESCAPE)){
             WindowShouldClose();
+
         }
+
+
         EndDrawing();
+
     }
+    UnloadTexture(fond);
     return 0;
 }
