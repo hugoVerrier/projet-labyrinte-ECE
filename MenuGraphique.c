6a6fc23dfdef50209d8a6199fc83068bd727043d
eliotrineau <eliot.rineau@edu.ece.fr>
2022-12-20 17:13:58 +0100

    ajout bouton quitter

--- MenuGraphique.c
+++ MenuGraphique.c
@@ -15,16 +15,26 @@ int fenetreMenu(){
     Image boutonImage = LoadImage("../ImagesMenu/parcheminhorizontal.png");
     ImageResize(&boutonImage,GetScreenWidth()/4,GetScreenHeight()/12);
     Texture2D bouton = LoadTextureFromImage(boutonImage);
+    Image quitterImage = LoadImage("../ImagesMenu/boutonQuitter.png");
+    ImageResize(&quitterImage,GetScreenWidth()/22,GetScreenHeight()/18);
+    Texture2D quitter = LoadTextureFromImage(quitterImage);
+    Font font = LoadFont("../Font/Terserah.ttf");
     SetTargetFPS(30);
     while (!WindowShouldClose()){
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
         DrawTextureEx(fond,(Vector2){0,0},0.0f,1.0f,WHITE);
+        DrawTextureEx(quitter,(Vector2){GetScreenWidth()-GetScreenWidth()/16,GetScreenHeight()/32},0.0f,1.0f,WHITE);
         DrawTextureEx(bouton,(Vector2){(GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8},0.0f,1.25f,WHITE);
         DrawTextureEx(bouton,(Vector2){(GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8+GetScreenWidth()/8},0.0f,1.25f,WHITE);
         DrawTextureEx(bouton,(Vector2){(GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8+2*GetScreenWidth()/8},0.0f,1.25f,WHITE);
         DrawTextureEx(bouton,(Vector2){(GetScreenWidth()/2)-GetScreenWidth()/8,GetScreenHeight()/8+3*GetScreenWidth()/8},0.0f,1.25f,WHITE);
+        DrawTextEx(font,"Nouvelle partie",(Vector2){(GetScreenWidth()/2-GetScreenWidth()/20),GetScreenHeight()/8+GetScreenHeight()/32},40,0,BLACK);
+        DrawTextEx(font,"Charger partie",(Vector2){(GetScreenWidth()/2-GetScreenWidth()/20),GetScreenHeight()/8+GetScreenWidth()/8+GetScreenHeight()/32},40,0,BLACK);
+        DrawTextEx(font,"Afficher regle",(Vector2){(GetScreenWidth()/2-GetScreenWidth()/20),GetScreenHeight()/8+2*GetScreenWidth()/8+GetScreenHeight()/32},40,0,BLACK);
+        DrawTextEx(font,"Credits",(Vector2){(GetScreenWidth()/2-GetScreenWidth()/50),GetScreenHeight()/8+3*GetScreenWidth()/8+GetScreenHeight()/32},40,0,BLACK);
+
         if(IsKeyPressed(KEY_ESCAPE)){
             WindowShouldClose();
 
