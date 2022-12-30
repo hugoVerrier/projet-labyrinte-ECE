8e587aff744fbae13425f9a089fcc09d88326db5
eliotrineau <eliot.rineau@edu.ece.fr>
2022-12-30 14:55:06 +0100

    test resolution probleme hugo

--- cmake-build-debug/CMakeFiles/projet_labyrinte_ECE.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/projet_labyrinte_ECE.dir/DependInfo.cmake
@@ -13,10 +13,18 @@ set(CMAKE_DEPENDS_CHECK_C
   )
 set(CMAKE_C_COMPILER_ID "AppleClang")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GRAPHICS_API_OPENGL_33"
+  "PLATFORM_DESKTOP"
+  )
+
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
   "../."
   "/opt/homebrew/Cellar/raylib/4.2.0/include"
+  "../raylib/src"
+  "../raylib/src/external/glfw/include"
   )
 
 # The set of dependency files which are needed:
@@ -25,6 +33,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/eliotrineau/CLionProjects/projet-labyrinte-ECE/cmake-build-debug/raylib/raylib/CMakeFiles/raylib.dir/DependInfo.cmake"
+  "/Users/eliotrineau/CLionProjects/projet-labyrinte-ECE/cmake-build-debug/raylib/raylib/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
