05d34bc8e9f4c3da1aa724502312af151530269e
eliotrineau <eliot.rineau@edu.ece.fr>
2022-12-30 17:24:28 +0100

    ajout des zones de textes pour les parametres d'une nouvelle partie

--- MenuGraphique.c
+++ MenuGraphique.c
@@ -9,7 +9,12 @@ void fenetreMenu(int* menu){
     ToggleFullscreen();
     Fenetre FenetreActuelle = MENU;
     int compteurFPS=0;
-    SetTargetFPS(60);
+    int compteurLettrePremierParam=0;
+    int compteurLettreDeuxiemeParam=0;
+    int compteurLettreTroisiemeParam=0;
+    char premierParam[CHAR_OUI_NON + 1] = "\0";
+    char deuxiemeParam[CHAR_CHOIXNBJOUEURS + 1] = "\0";
+    char troisiemeParam[CHAR_CHOIX_COULEUR_PION + 1] = "\0";
 
 
     // chargement et redimension des textures MENU
@@ -19,11 +24,12 @@ void fenetreMenu(int* menu){
 
     Image boutonImage = LoadImage("../ImagesMenu/parcheminhorizontal.png");
     ImageResize(&boutonImage,GetScreenWidth()/4,GetScreenHeight()/12);
-    Texture2D bouton = LoadTextureFromImage(boutonImage)
-            ;
+    Texture2D bouton = LoadTextureFromImage(boutonImage);
+
     Image quitterImage = LoadImage("../ImagesMenu/boutonQuitter.png");
     ImageResize(&quitterImage,GetScreenWidth()/22,GetScreenHeight()/18);
     Texture2D quitter = LoadTextureFromImage(quitterImage);
+
     Font font = LoadFont("../Font/Terserah.ttf");
 
 
@@ -64,12 +70,114 @@ void fenetreMenu(int* menu){
     UnloadImage(XavierImage);
 
     int fin = 0; // fin inutile juste histoire de pas fermer la fenetre
+
+
+    SetTargetFPS(60);
     while (!fin ){
         Vector2 positionSouris;
         positionSouris = GetMousePosition();
+        Rectangle TexteBoutonRE = { GetScreenWidth()/6+GetScreenWidth()/40, GetScreenHeight()/6+2.5*GetScreenHeight()/40, GetScreenHeight()/8, GetScreenHeight()/8 };
+        Rectangle TexteBoutonNJ = { GetScreenWidth()/6+GetScreenWidth()/40, GetScreenHeight()/6+10*GetScreenHeight()/40, GetScreenHeight()/8, GetScreenHeight()/8 };
+        Rectangle TexteBoutonCC = { GetScreenWidth()/6+GetScreenWidth()/40, GetScreenHeight()/6+18*GetScreenHeight()/40, GetScreenHeight()/8, GetScreenHeight()/8 };
+        bool sourisSurTexteRE = false;
+        bool sourisSurTexteNJ = false;
+        bool sourisSurTexteCC = false;
+
+
+        if (CheckCollisionPointRec(GetMousePosition(), TexteBoutonRE)) sourisSurTexteRE = true;
+        else sourisSurTexteRE = false;
+        if (CheckCollisionPointRec(GetMousePosition(), TexteBoutonNJ)) sourisSurTexteNJ = true;
+        else sourisSurTexteNJ = false;
+        if (CheckCollisionPointRec(GetMousePosition(), TexteBoutonCC)) sourisSurTexteCC = true;
+        else sourisSurTexteCC = false;
+
+        if (sourisSurTexteRE)
+        {
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+            int key = GetCharPressed();
+            while (key > 0){
+                if ((key >= 32) && (key <= 125) && (compteurLettrePremierParam < CHAR_OUI_NON))
+                {
+                    premierParam[compteurLettrePremierParam] = (char)key;
+                    premierParam[compteurLettrePremierParam+1] = '\0';
+                    compteurLettrePremierParam++;
+                }
+                key = GetCharPressed();
+            }
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                compteurLettrePremierParam--;
+                if (compteurLettrePremierParam < 0) compteurLettrePremierParam = 0;
+                premierParam[compteurLettrePremierParam] = '\0';
+            }
+        }
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+
+        if (sourisSurTexteRE) compteurFPS++;
+        else compteurFPS = 0;
+
+
+
+
+
+        if (sourisSurTexteNJ)
+        {
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+            int key = GetCharPressed();
+            while (key > 0){
+                if ((key >= 32) && (key <= 125) && (compteurLettreDeuxiemeParam < CHAR_CHOIXNBJOUEURS))
+                {
+                    deuxiemeParam[compteurLettreDeuxiemeParam] = (char)key;
+                    deuxiemeParam[compteurLettreDeuxiemeParam+1] = '\0';
+                    compteurLettreDeuxiemeParam++;
+                }
+                key = GetCharPressed();
+            }
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                compteurLettreDeuxiemeParam--;
+                if (compteurLettreDeuxiemeParam < 0) compteurLettreDeuxiemeParam = 0;
+                deuxiemeParam[compteurLettreDeuxiemeParam] = '\0';
+            }
+        }
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+
+        if (sourisSurTexteNJ) compteurFPS++;
+        else compteurFPS = 0;
+
+
+
+
+
+
+        if (sourisSurTexteCC)
+        {
+            SetMouseCursor(MOUSE_CURSOR_IBEAM);
+            int key = GetCharPressed();
+            while (key > 0){
+                if ((key >= 32) && (key <= 125) && (compteurLettreTroisiemeParam < CHAR_CHOIX_COULEUR_PION))
+                {
+                    troisiemeParam[compteurLettreTroisiemeParam] = (char)key;
+                    troisiemeParam[compteurLettreTroisiemeParam+1] = '\0';
+                    compteurLettreTroisiemeParam++;
+                }
+                key = GetCharPressed();
+            }
+            if (IsKeyPressed(KEY_BACKSPACE))
+            {
+                compteurLettreTroisiemeParam--;
+                if (compteurLettreTroisiemeParam < 0) compteurLettreTroisiemeParam = 0;
+                troisiemeParam[compteurLettreTroisiemeParam] = '\0';
+            }
+        }
+        else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+
+        if (sourisSurTexteCC) compteurFPS++;
+        else compteurFPS = 0;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        compteurFPS++;
+
 
         switch (FenetreActuelle) {
 
@@ -121,46 +229,31 @@ void fenetreMenu(int* menu){
                 DrawTextureEx(fond,(Vector2){0,0},0.0f,1.0f,WHITE);
                 DrawTextureEx(retourMenu,(Vector2){GetScreenWidth()-GetScreenWidth()/16,GetScreenHeight()/32},0.0f,1.0f,WHITE);
                 DrawRectangle(GetScreenWidth()/6,GetScreenHeight()/6,GetScreenWidth()-2*GetScreenWidth()/6,GetScreenHeight()-2*GetScreenHeight()/6,BEIGE);
-                DrawRectangle(GetScreenWidth()/6+GetScreenWidth()/4-GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40,GetScreenHeight()/10,GetScreenHeight()/20,BROWN);
-                DrawRectangle(GetScreenWidth()/6+GetScreenWidth()/2-GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40,GetScreenHeight()/10,GetScreenHeight()/20,BROWN);
-                DrawTextEx(font,"Regle enfant :\t\t\t\t\t\t\t\tOui\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tNon",(Vector2){GetScreenWidth()/6+GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40},30,2,BLACK);
-                DrawTextEx(font,"Nombre de Joueurs (2, 3 ou 4 Joueurs)",(Vector2){GetScreenWidth()/6+GetScreenWidth()/40,GetScreenHeight()/6+6*GetScreenHeight()/40},30,2,BLACK);
-                
+                if (sourisSurTexteRE){
+                    DrawRectangle(TexteBoutonRE.x,TexteBoutonRE.y,TexteBoutonRE.width,TexteBoutonRE.height,WHITE);
+                }
+                else DrawRectangle(TexteBoutonRE.x,TexteBoutonRE.y,TexteBoutonRE.width,TexteBoutonRE.height,BROWN);
+                DrawText(premierParam, (int)TexteBoutonRE.x + 5, (int)TexteBoutonRE.y + 8, 40, MAROON);
+                if (sourisSurTexteNJ){
+                    DrawRectangle(TexteBoutonNJ.x,TexteBoutonNJ.y,TexteBoutonNJ.width,TexteBoutonNJ.height,WHITE);
+                }
+                else DrawRectangle(TexteBoutonNJ.x,TexteBoutonNJ.y,TexteBoutonNJ.width,TexteBoutonNJ.height,BROWN);
+                DrawText(deuxiemeParam, (int)TexteBoutonNJ.x + 5, (int)TexteBoutonNJ.y + 8, 40, MAROON);
+
+                if (sourisSurTexteCC){
+                    DrawRectangle(TexteBoutonCC.x,TexteBoutonCC.y,TexteBoutonCC.width,TexteBoutonCC.height,WHITE);
+                }
+                else DrawRectangle(TexteBoutonCC.x,TexteBoutonCC.y,TexteBoutonCC.width,TexteBoutonCC.height,BROWN);
+                DrawText(troisiemeParam, (int)TexteBoutonCC.x + 5, (int)TexteBoutonCC.y + 8, 40, MAROON);
+                DrawTextEx(font,"Regle enfant : (Oui ou Non)",(Vector2){GetScreenWidth()/6+GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40},30,2,BLACK);
+                DrawTextEx(font,"Nombre de Joueurs (2, 3 ou 4 Joueurs)",(Vector2){GetScreenWidth()/6+GetScreenWidth()/40,GetScreenHeight()/6+8*GetScreenHeight()/40},30,2,BLACK);
+                DrawTextEx(font,"Choisir la couleur de votre pion (Jaune,Bleu,Rouge,Vert)",(Vector2){GetScreenWidth()/6+GetScreenWidth()/40,GetScreenHeight()/6+16*GetScreenHeight()/40},30,2,BLACK);
                 // a faire
                 if (IsKeyPressed(KEY_ESCAPE) ||  (positionSouris.x >= GetScreenWidth()-GetScreenWidth()/16) && (positionSouris.x <= GetScreenWidth()-GetScreenWidth()/16 + quitterImage.width) && (positionSouris.y >= GetScreenHeight()/32) && (positionSouris.y <= GetScreenHeight()/32 +quitterImage.height) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)))
                 {
                     FenetreActuelle = MENU;
                 }
-                else if ((positionSouris.x >=GetScreenWidth()/6+GetScreenWidth()/4-GetScreenWidth()/40) && (positionSouris.x <=GetScreenWidth()/6+GetScreenWidth()/4-GetScreenWidth()/40+GetScreenHeight()/10) && (positionSouris.y >= GetScreenHeight()/6+GetScreenHeight()/40) && (positionSouris.y <= GetScreenHeight()/6+GetScreenHeight()/40+GetScreenHeight()/20) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))){
-                    FenetreActuelle = NOUVELLE_PARTIE_ENFANT_OUI;
-                }
-                else if ((positionSouris.x >=GetScreenWidth()/6+GetScreenWidth()/2-GetScreenWidth()/40) && (positionSouris.x <=GetScreenWidth()/6+GetScreenWidth()/2-GetScreenWidth()/40+GetScreenHeight()/10) && (positionSouris.y >= GetScreenHeight()/6+GetScreenHeight()/40) && (positionSouris.y <= GetScreenHeight()/6+GetScreenHeight()/40+GetScreenHeight()/20) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))){
-                    FenetreActuelle = NOUVELLE_PARTIE_ENFANT_NON;
-                }
-            }break;
-            case NOUVELLE_PARTIE_ENFANT_OUI:{
-                DrawTextureEx(fond,(Vector2){0,0},0.0f,1.0f,WHITE);
-                DrawTextureEx(retourMenu,(Vector2){GetScreenWidth()-GetScreenWidth()/16,GetScreenHeight()/32},0.0f,1.0f,WHITE);
-                DrawRectangle(GetScreenWidth()/6,GetScreenHeight()/6,GetScreenWidth()-2*GetScreenWidth()/6,GetScreenHeight()-2*GetScreenHeight()/6,BEIGE);
-                DrawRectangle(GetScreenWidth()/6+GetScreenWidth()/4-GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40,GetScreenHeight()/10,GetScreenHeight()/20,GREEN);
-                DrawRectangle(GetScreenWidth()/6+GetScreenWidth()/2-GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40,GetScreenHeight()/10,GetScreenHeight()/20,RED);
-                DrawTextEx(font,"Regle enfant :\t\t\t\t\t\t\t\tOui\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tNon",(Vector2){GetScreenWidth()/6+GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40},30,2,BLACK);
-                if (IsKeyPressed(KEY_ESCAPE) ||  (positionSouris.x >= GetScreenWidth()-GetScreenWidth()/16) && (positionSouris.x <= GetScreenWidth()-GetScreenWidth()/16 + quitterImage.width) && (positionSouris.y >= GetScreenHeight()/32) && (positionSouris.y <= GetScreenHeight()/32 +quitterImage.height) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)))
-                {
-                    FenetreActuelle = MENU;
-                }
-            }break;
-            case NOUVELLE_PARTIE_ENFANT_NON:{
-                DrawTextureEx(fond,(Vector2){0,0},0.0f,1.0f,WHITE);
-                DrawTextureEx(retourMenu,(Vector2){GetScreenWidth()-GetScreenWidth()/16,GetScreenHeight()/32},0.0f,1.0f,WHITE);
-                DrawRectangle(GetScreenWidth()/6,GetScreenHeight()/6,GetScreenWidth()-2*GetScreenWidth()/6,GetScreenHeight()-2*GetScreenHeight()/6,BEIGE);
-                DrawRectangle(GetScreenWidth()/6+GetScreenWidth()/4-GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40,GetScreenHeight()/10,GetScreenHeight()/20,RED);
-                DrawRectangle(GetScreenWidth()/6+GetScreenWidth()/2-GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40,GetScreenHeight()/10,GetScreenHeight()/20,GREEN);
-                DrawTextEx(font,"Regle enfant :\t\t\t\t\t\t\t\tOui\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tNon",(Vector2){GetScreenWidth()/6+GetScreenWidth()/40,GetScreenHeight()/6+GetScreenHeight()/40},30,2,BLACK);
-                if (IsKeyPressed(KEY_ESCAPE) ||  (positionSouris.x >= GetScreenWidth()-GetScreenWidth()/16) && (positionSouris.x <= GetScreenWidth()-GetScreenWidth()/16 + quitterImage.width) && (positionSouris.y >= GetScreenHeight()/32) && (positionSouris.y <= GetScreenHeight()/32 +quitterImage.height) && (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)))
-                {
-                    FenetreActuelle = MENU;
-                }
+
             }break;
             case CHARGER_PARTIE:{
                 DrawTextureEx(fond,(Vector2){0,0},0.0f,1.0f,WHITE);
@@ -213,3 +306,5 @@ void fenetreMenu(int* menu){
     }
     CloseWindow();
 }
+
+
