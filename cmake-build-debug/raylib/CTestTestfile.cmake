d7cc7e27c59c5956562450ff602f7187ddcd63fc
eliotrineau <eliot.rineau@edu.ece.fr>
2023-01-03 10:07:57 +0100

    menu presque terminé

---
+++ cmake-build-debug/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/eliotrineau/CLionProjects/projet-labyrinte-ECE/raylib
+# Build directory: /Users/eliotrineau/CLionProjects/projet-labyrinte-ECE/cmake-build-debug/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
