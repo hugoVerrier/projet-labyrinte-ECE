71fd5eeba3ede223d00c5ffc85445d605a643fbf
Xavier Ogandagawora <xavier.ogandagawora@edu.ece.fr>
2023-01-11 15:57:51 +0100

    Test Git

--- cmake-build-debug/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/eliotrineau/CLionProjects/projet-labyrinte-ECE/raylib/src
-# Build directory: /Users/eliotrineau/CLionProjects/projet-labyrinte-ECE/cmake-build-debug/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
