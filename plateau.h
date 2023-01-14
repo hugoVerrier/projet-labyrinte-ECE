569703626750aa3879aefc35222de3e1839dd06d
hugoVerrier <hugo.verrier@edu.ece.fr>
2023-01-14 16:13:13 +0100

    le plateau se melange et la tuile supplémentaire peut etre tourner

--- plateau.h
+++ plateau.h
@@ -15,14 +15,15 @@
 
 
 
-void affichageConsol();
-void AfficchageConsolPlateau(Cases PlateauAfficher[LIGNE][COLONNE]);
+void affichageConsol(Cases plateau[NBCASE],Cases PlateauAfficher[LIGNE][COLONNE],Cases PlateauAttend[2]);
+void AfficchageConsolPlateau(Cases PlateauAfficher[LIGNE][COLONNE],Cases PlateauAttend[2]);
 void MelangeTab(Cases* plateau);
 void RemplirTabCase(Cases* plateau);
 void RemplirPlateauLFixe(Cases PlateauAfficher[LIGNE][COLONNE]);
 void RemplirPlateauTFixe(Cases PlateauAfficher[LIGNE][COLONNE]);
-void RemplirTabCaseAfficher(Cases plateau[NBCASE],Cases PlateauAfficher[LIGNE][COLONNE]);
-
+void RemplirTabCaseAfficher(Cases plateau[NBCASE],Cases PlateauAfficher[LIGNE][COLONNE],Cases PlateauAttend[2]);
+Texture2D ChoixDeCases(Cases PlateauAfficher[LIGNE][COLONNE],int x,int y,Texture2D I1,Texture2D I2,Texture2D L1,Texture2D L2,Texture2D L3,Texture2D L4,Texture2D T1,Texture2D T2,Texture2D T3,Texture2D T4);
+Texture2D ChoixDeCasesDehors(Cases PlateauAttend[2],int x,Texture2D I1,Texture2D I2,Texture2D L1,Texture2D L2,Texture2D L3,Texture2D L4,Texture2D T1,Texture2D T2,Texture2D T3,Texture2D T4);
 void AffichageRaylib();
 
 #endif //PROJET_LABYRINTE_ECE_PLATEAU_H
